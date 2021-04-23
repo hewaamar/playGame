3dd7d8b3ac801de0402e18b2b2c26a9dfda0fcb7
Hewan Amare <hewaamar557@ed.amdsb.ca>
2021-04-23 10:21:31 -0400

    Add project files.

---
+++ playGame/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿
+namespace playGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.playButton = new System.Windows.Forms.Button();
+            this.counter = new System.Windows.Forms.Label();
+            this.go = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // playButton
+            // 
+            this.playButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.playButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.playButton.Font = new System.Drawing.Font("OCR A Extended", 28F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.playButton.Location = new System.Drawing.Point(174, 71);
+            this.playButton.Name = "playButton";
+            this.playButton.Size = new System.Drawing.Size(425, 161);
+            this.playButton.TabIndex = 0;
+            this.playButton.Text = "PLAY GAME";
+            this.playButton.UseVisualStyleBackColor = true;
+            this.playButton.Click += new System.EventHandler(this.playButton_Click_1);
+            // 
+            // counter
+            // 
+            this.counter.AutoEllipsis = true;
+            this.counter.Font = new System.Drawing.Font("Lucida Sans Typewriter", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.counter.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.counter.Location = new System.Drawing.Point(218, 294);
+            this.counter.Name = "counter";
+            this.counter.Size = new System.Drawing.Size(323, 55);
+            this.counter.TabIndex = 1;
+            this.counter.Text = "Game starts in 3";
+            this.counter.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.counter.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // go
+            // 
+            this.go.AutoSize = true;
+            this.go.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.go.Location = new System.Drawing.Point(186, 120);
+            this.go.Name = "go";
+            this.go.Size = new System.Drawing.Size(413, 163);
+            this.go.TabIndex = 2;
+            this.go.Text = "GO!!!";
+            this.go.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.go);
+            this.Controls.Add(this.counter);
+            this.Controls.Add(this.playButton);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button playButton;
+        private System.Windows.Forms.Label counter;
+        private System.Windows.Forms.Label go;
+    }
+}
+
---
+++ playGame/Form1.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace playGame
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void playButton_Click_1(object sender, EventArgs e)
+        {
+
+            // make play button click sound
+            SoundPlayer playButtonSound = new SoundPlayer(Properties.Resources.playButtonSound);
+            playButtonSound.Play();
+
+            // make play button disappear
+            playButton.Visible = false;
+
+            // change the countdown number from 3 to 2
+            counter.Text = "Game starts in 2";
+
+            // play beep sound
+            SoundPlayer counterBeep = new SoundPlayer(Properties.Resources.counterBeep);
+            counterBeep.Play();
+
+            // wait 1 second
+            Refresh();
+            Thread.Sleep(1500);
+
+            // change the countdown number from 2 to 1
+            counter.Text = "Game starts in 1";
+
+            // play beep sound
+            counterBeep.Play();
+
+            // wait 1 second
+            Refresh();
+            Thread.Sleep(1500);
+
+            // change the countdown number from 2 to 1
+            counter.Text = "Game starts in 0";
+
+            // play beep sound
+            counterBeep.Play();
+
+            // wait 1 second
+            Refresh();
+            Thread.Sleep(1500);
+
+            //make counter disappear
+            counter.Visible = false;
+
+            // play beep sound
+            counterBeep.Play();
+
+            //make "GO!!!" appear
+            go.Visible = true;
+
+            // play GO sound
+            SoundPlayer goSound = new SoundPlayer(Properties.Resources.goSound);
+            goSound.Play();
+
+            // make "GO!!! change colour"
+            go.BackColor = Color.YellowGreen;
+
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
